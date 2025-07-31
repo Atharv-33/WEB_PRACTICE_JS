890e73341e74fb742bd22a6ef62c478d8b79c688
Atharv-33 <atharveducat@gmail.com>
2025-07-31 21:06:35 +0530

    Add files via upload

---
+++ third.js
@@ -0,0 +1,48 @@
+/* undefined*/ 
+let a;
+console.log(a);
+console.log(typeof a);
+
+
+/* null in javascript */
+let b = null;
+ console.log(b);
+ console.log(typeof b);
+
+ // numbers in javascript
+ let dec = 10;
+ console.log(dec);
+ console.log( typeof dec);
+
+ let float = 17.77;
+ console.log(float);
+ console.log(typeof float);
+
+ //strings in javascript 
+
+ let str = "Hello this is Atharv ";
+ console.log(str);
+ console.log(typeof str);
+
+ // booleans in javascript
+ let bool = true;
+ console.log(bool);
+ console.log(typeof bool);
+
+ // objects in javascript
+
+ const student={
+    name:"Abcd",
+    marks:100,
+ };
+
+ console.log(student.name);
+ console.log(student.marks);
+ console.log(typeof student);
+
+ // symbols in javascript 
+
+ const sym1 = Symbol("abcd");
+ console.log(typeof sym1);
+
+ console.log(Symbol("a") == Symbol("a"));
